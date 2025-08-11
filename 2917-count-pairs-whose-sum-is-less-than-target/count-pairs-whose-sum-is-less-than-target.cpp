1f01101e3996620c53411dbabbe89591bcf23a70
Mohit Kumar Agarwal <rakhiiagrawal946@gmail.com>
2025-08-11 18:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (15.01%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -2,19 +2,33 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
           
-          //Approach 1: Using brute force approach 
-          int p=0;
-          for(int i=0;i<nums.size();i++)
-          {
-             for(int j = i+1;j<nums.size();j++)
-             {
-                  if(nums[i]+nums[j]<target)
-                  {
-                     p++;
-                  }
-             }
-          }
+          //Approach 1: Using brute force approach  T.C= O(n*n)
+        //   int p=0;
+        //   for(int i=0;i<nums.size();i++)
+        //   {
+        //      for(int j = i+1;j<nums.size();j++)
+        //      {
+        //           if(nums[i]+nums[j]<target)
+        //           {
+        //              p++;
+        //           }
+        //      }
+        //   }
 
-          return p;
+        //   return p;
+
+        // Coming to the optimal approach
+           sort(nums.begin(), nums.end());
+    int n = nums.size();
+    long long count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int needed = target - nums[i]; // nums[j] < needed
+        auto it = lower_bound(nums.begin() + i + 1, nums.end(), needed);
+        int j = it - nums.begin();
+        count += (j - (i + 1));
+    }
+
+    return count;
     }
 };
