629ea084ffd0a50c1834f42e96ed782812699830
Mohit Kumar Agarwal <rakhiiagrawal946@gmail.com>
2025-08-14 18:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (24.06%) - LeetSync

--- 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums) {
-         int sum=0;
-         for(int i=0;i<nums.size();i++)
-         {
-             int start = max(0,i-nums[i]);
-               for(int j = start;j<=i;j++)
-               {
-                 sum+= nums[j];
-               }
-         }
-
-         return sum;
+        
+    int n = nums.size();
+    vector<int> prefix(n, 0);
+    
+    // Step 1: Build prefix sum
+    prefix[0] = nums[0];
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+    
+    int totalSum = 0;
+    
+    // Step 2: For each i, find subarray sum using prefix sums
+    for (int i = 0; i < n; i++) {
+        int start = max(0, i - nums[i]);
+        if (start == 0) {
+            totalSum += prefix[i];
+        } else {
+            totalSum += prefix[i] - prefix[start - 1];
+        }
+    }
+    
+    return totalSum;
     }
 };
