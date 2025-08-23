0f6f19f6ba9756702ea9b2dbfca3f0db439ff83a
Mohit Kumar Agarwal <rakhiiagrawal946@gmail.com>
2025-08-23 14:22:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -25,7 +25,23 @@ public:
 
         // Method 2:  Memoization method 
 
-        unordered_map<int , int> dp;
-        return solve(n,dp);
+        // unordered_map<int , int> dp;
+        // return solve(n,dp);
+
+        // Method 3: Tabulation - Bottom Up approach 
+         
+         if(n==0|| n==1)
+         return 1;
+
+         vector<int>dp(n+1,0);
+
+         dp[0]=1, dp[1]=1;
+
+         for(int i=2;i<=n;i++)
+         {
+              dp[i] = dp[i-1]+dp[i-2];
+         }
+
+        return  dp[n];
     }
 };
